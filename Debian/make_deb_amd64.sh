7da7b0eb45523db58e5f854d8c024ac9aa72c88c
neurolabusc <rorden@sc.edu>
2021-09-16 15:50:50 -0400

    Initial Debian support

---
+++ Debian/make_deb_amd64.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# Fail if anything not planed to go wrong, goes wrong
+set -eu
+sw=mricrogl
+ver=1.2.20210909
+arch=amd64
+#set widgetset set for default(QT5)
+wigetset=
+sw=${sw}${wigetset}
+
+pkg=${sw}_${ver}_${arch}
+#deb=${pkg}.deb
+exePath=${pkg}/usr/bin/
+appPath=${pkg}/usr/share/applications/
+docPath=${pkg}/usr/share/doc/${sw}/
+iconPath=${pkg}/usr/share/icons/hicolor/scalable/apps/
+manPath=${pkg}/usr/share/man/man1/
+man=${sw}.1.gz
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $SCRIPT_DIR
+
+if [ -z "$wigetset" ]
+then
+      lazbuild --ws=qt5 ../MRIcroGL.lpi
+else
+      lazbuild --ws=gtk2 ../MRIcroGL.lpi
+fi
+
+#remove previous package files
+rm -rf ${pkg}
+#copy all required files
+#copy control file
+mkdir -p ${pkg}/DEBIAN
+cp ./${sw}-control.txt ${pkg}/DEBIAN/control
+#copy executable
+mkdir -p ${exePath}
+cp ../MRIcroGL ${exePath}${sw}
+#copy desktop file
+mkdir -p ${appPath}
+cp ${sw}.desktop ${appPath}${sw}.desktop
+echo cp ${sw}.desktop ${appPath}${sw}.desktop
+#copy icon svg
+mkdir -p ${iconPath}
+cp ${sw}.svg ${iconPath}${sw}.svg
+
+#copy documents
+mkdir -p ${docPath}
+cp ../license.txt ${docPath}copyright
+#copy manual
+mkdir -p ${manPath}
+cp ${man} ${manPath}${man}
+
+dpkg-deb --build --root-owner-group ${pkg}
+#remove temporary files
+rm -rf ${pkg}
+
+echo Test the package:
+echo sudo dpkg -I ${pkg}.deb
+echo sudo dpkg -c ${pkg}.deb
+echo sudo dpkg -i ${pkg}.deb
+exit 0
+
+
+
+
+
---
+++ Debian/make_deb_data.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#Create Debian package for all versions of MRIcroGL
+
+# Fail if anything not planed to go wrong, goes wrong
+set -eu
+sw=mricrogl-data
+ver=1.2.20210909
+pkg=${sw}_${ver}_all
+#deb=${pkg}.deb
+resourcePath=${pkg}/usr/share/mricrogl/
+
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $SCRIPT_DIR
+
+#remove previous package files
+rm -rf ${pkg}
+#copy all required files
+mkdir -p ${pkg}/DEBIAN
+cp ./data-control.txt ${pkg}/DEBIAN/control
+mkdir -p ${resourcePath}
+cp -a ../Resources/. ${resourcePath}
+#Linux does not need Windows resources python36.dll and python36.zip
+rm -rf ${resourcePath}python*.*
+
+dpkg-deb --build --root-owner-group ${pkg}
+#remove temporary files
+rm -rf ${pkg}
+exit 0
+
+
+
+
+
